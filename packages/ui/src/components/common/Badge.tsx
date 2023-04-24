d8e7a4f2cfd93c53b7d038648f5812ba589840e9
MatheusLittig <littig.works@gmail.com>
2023-04-24 11:27:07 -0400

    refactor: adding custom border for badge component

--- packages/ui/src/components/common/Badge.tsx
+++ packages/ui/src/components/common/Badge.tsx
@@ -1,7 +1,7 @@
 import { VariantProps, cva } from "class-variance-authority"
 
 const className = cva(
-  "flex text-gray-50 items-center gap-2 min-w-[40px] min-h-[2rem] w-auto px-2 rounded ",
+  "flex text-gray-50 items-center gap-2 min-w-[40px] w-[calc(100%_-_24px)] rounded-l min-h-[2rem] px-2",
   {
     variants: {
       variant: {
@@ -18,7 +18,23 @@ type BadgeProps = React.HTMLAttributes<HTMLSpanElement> &
   VariantProps<typeof className>
 
 const Badge = ({ variant, ...props }: BadgeProps) => {
-  return <span className={className({ variant })} {...props} />
+  return (
+    <span className="relative flex items-center h-12 w-full">
+      <div className={className({ variant })} {...props} />
+      <div
+        style={{
+          position: "absolute",
+          right: "0",
+          width: "0",
+          height: "0",
+          borderBottom: "16px solid transparent",
+          borderLeft: "16px solid #133a6f",
+          borderRight: "16px solid #133a6f",
+          transform: "rotate(-90deg)",
+        }}
+      />
+    </span>
+  )
 }
 
 export default Badge
