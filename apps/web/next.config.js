8fdafa98a0d2981420f624be2a109121e69753dc
MatheusLittig <littig.works@gmail.com>
2023-04-26 21:00:54 -0400

    refactor: updating pnpm lock and removing type check on build

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@woovi/ui", "@woovi/resources"],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   experimental: {
     appDir: true,
   },
